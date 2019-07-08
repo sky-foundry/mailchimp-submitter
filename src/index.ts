66d201c7aaa18847f97b269b5e0193ed029ef4a1
Ari Seyhun <ariseyhun@Aris-MBP.localdomain>
2019-07-08 13:35:04 +0930

    feat: add callbacks `beforeSubmit` and `callback`

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,37 @@ import getFieldValueByName from './utils/getFieldByName'
 import getFormFields from './utils/getFormFields'
 import objToUrlParams from './utils/objToUrlParams'
 
-export default function() {
+interface Parameters {
+  beforeSubmit?: (formEl: Element) => boolean | Promise<boolean> | void
+  callback?: (formEl: Element, result: object) => void
+}
+
+export default function({ beforeSubmit, callback }: Parameters = {}) {
   const mcForms = document.querySelectorAll('form[data-mc]')
   for (let i = 0; i < mcForms.length; ++i) {
     const mcForm = mcForms.item(i)
-    mcForm.addEventListener('submit', ev => {
+    mcForm.addEventListener('submit', async ev => {
       ev.preventDefault()
 
       const formEl = ev.target as HTMLFormElement
       if (formEl == null) {
         return false
+      }
+
+      const customEventBeforeSubmit = new CustomEvent('mcBeforeSubmit')
+      formEl.dispatchEvent(customEventBeforeSubmit)
+
+      if (beforeSubmit) {
+        const result = beforeSubmit(formEl)
+        let finalResult: boolean | void
+        if (result instanceof Promise) {
+          finalResult = await result
+        } else {
+          finalResult = result
+        }
+        if (finalResult === false) {
+          return
+        }
       }
 
       const fields: HTMLInputElement[] = getFormFields(
@@ -26,18 +47,22 @@ export default function() {
         .substr(2, 9)}`
       const win = window as { [key: string]: any }
 
-      win[callbackFnId] = function(result: any) {
+      win[callbackFnId] = (result: any) => {
         if (formEl == null) {
           return
         }
 
         const customEvent = new CustomEvent('mcCallback', { detail: result })
         formEl.dispatchEvent(customEvent)
+
+        if (callback) {
+          callback(formEl, result)
+        }
+
         delete win[callbackFnId]
       }
 
       let url = `https://${mcProject}.${mcDatacenter}.list-manage.com/subscribe/post-json?c=${callbackFnId}&`
-
       url += objToUrlParams(
         fields.reduce(
           (obj: { [key: string]: any }, field: HTMLInputElement) => {
